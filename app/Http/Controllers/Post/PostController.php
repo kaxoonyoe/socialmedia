e0acb05f3710dd22d649d4446accf552f5418028
kaxoonyoe <aungkophonemyint@gmail.com>
2021-01-26 15:52:53 +0630

    'post_page_start_up'

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -10,4 +10,7 @@ class PostController extends Controller
     public function index(){
         return view('posts.index');
     }
+    public function store(Request $request){
+
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,28 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            POSTS
+
+            <form action="{{ route('posts') }}" method="POST">
+
+                @csrf
+
+                <div class="mb-4">
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea name="body" id="body" cols="30" rows="4" class="w-full bg-gray-100 rounded-lg p-4 border-2" placeholder="What is on your mind?"></textarea>
+                </div>
+
+                @error('body')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}
+                    </div>
+                @enderror
+
+                <div>
+                    <button type="submit" class="text-white bg-blue-500 rounded font-medium px-4 py-2">Post</button>
+                </div>
+
+            </form>
+
         </div>
     </div>
 @endsection
